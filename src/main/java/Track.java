158a585f0cc6e501b3eca3ac542d981f3b287360
Ferenc Bondár <ferenc.bondar@ibm.com>
2021-07-30 10:59:12 +0200

    X moves GREEN

--- src/main/java/Track.java
+++ src/main/java/Track.java
@@ -1,9 +1,25 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Track {
+
+    private static final int SIZE = 3;
+
+    private int[][] content = new int[SIZE][SIZE];
+
     // 0 = empty
     // 1 = X
     // 2 = O
+
+    public Track() {
+        for (int row = 0; row < content.length; row++) {
+            for (int col = 0; col < content.length; col++) {
+                content[row][col] = 0;
+            }
+        }
+    }
+
     public int[][] getTrackContent() {
-        return new int[0][];
+        return content;
     }
 
     public String getGameBoard() {
@@ -11,6 +27,10 @@ public class Track {
     }
 
     public void XMoves() {
-
+        int randomRow = ThreadLocalRandom.current()
+                                         .nextInt(0,SIZE-1);
+        int randomColumn = ThreadLocalRandom.current()
+                                         .nextInt(0,SIZE-1);
+        content[randomRow][randomColumn] = 1;
     }
 }
